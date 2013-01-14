e118e0fe00150c6407fe54040bce0db31506a942
mat <mattlke@yahoo.fr>
2013-01-14 23:44:39 +0100

    clean

--- parse_expl/parse_expl.py
+++ parse_expl/parse_expl.py
@@ -41,9 +41,13 @@ def get_recherche_result(idx_page):
 
 #write_html_file()
 
-def parse_annonce_in_recherche(soup):
+def parse_annonce_in_search_page(annonce_soup):
+    '''renvoie un objet avec les champs :
+    short_title, surface, type, nb_rooms, full_url, price, nb_pics'''
 
     res = Obj()
+    1
+    soup = annonce_soup
 
     def get_sub_str(loc_soup, key, class_name):
 
@@ -78,10 +82,10 @@ def parse_annonce_in_recherche(soup):
     res.price = a_list[0].string.replace(u'\xa0', ' ')
 
     #print [res.total]        
-    res.nb_photos_str = soup.find_all('div', 'sprEI legende')[0].string
-    res.nb_photos = int(res.nb_photos_str.split(' ')[0])
+    res.nb_pics_str = soup.find_all('div', 'sprEI legende')[0].string
+    res.nb_pics = int(res.nb_pics_str.split(' ')[0])
 
-    print 'nb_photos', [res.nb_photos_str]
+    #print 'nb_photos', [res.nb_photos_str]
 
     bien_soup_list = soup.find_all('div', 'bien')
     assert len(bien_soup_list) == 1
@@ -89,19 +93,12 @@ def parse_annonce_in_recherche(soup):
 
     return res
 
-def parse_annonce_page(full_url):
+def parse_annonce_own_page(own_url):
 
     res = Obj()
 
-    #full_url = os.path.join(root_url, sub_url)   
-
-    #print 'full_url', full_url    
-
-    f = urllib.urlopen(full_url)    
-
-    #html = open('explorimmo_annonce.html', 'rb').read()
+    f = urllib.urlopen(own_url)
     html = f.read()
-
     soup = BeautifulSoup(html)
 
     l = soup.find_all('div', id='bloc-vue-detail')
@@ -109,20 +106,18 @@ def parse_annonce_page(full_url):
     l2 = l[0].find_all('h1')
     assert len(l2) == 1
 
+    #1 : le titre
+
     res.full_title = l2[0].string.replace('\n', '').strip(' ').replace('  ', '')
     res.full_title = res.full_title.replace(u'\xa0', '')
-    
-    #print 'full title', res.full_title.encode('utf-8'), [res.full_title]
 
-    #maintenant la description
+    #2 : la description
 
     l = soup.find_all('div', id='detailDescriptionTxt')
     assert len(l) == 1
 
     l2 = l[0].find_all('p')
     assert len(l2) == 1
-
-    #print l2[0].prettify()
 
     desc = ''
 
@@ -132,74 +127,45 @@ def parse_annonce_page(full_url):
 
     res.description = desc
 
-    #les liens vers les photos
+    #3 : les liens vers les photos
+
     l = soup.find_all('div', id='detailSlideLinks')
     assert len(l) == 1
     l2 = l[0].find_all('img')
 
     res.image_src_list = [x['src'] for x in l2]
 
-    #print res.image_src_list
-
-    #print res.description.encode('utf-8')
+    #print 'img_src_list', res.image_src_list
 
     return res
 
-def parse_annonce_full(annonce):
+def parse_annonce_full(annonce_soup):
 
-    res = parse_annonce_in_recherche(annonce)
+    res = parse_annonce_in_search_page(annonce_soup)
 
-    detailed_res = parse_annonce_page(res.full_url)
+    detailed_res = parse_annonce_own_page(res.full_url)
 
     res.full_title = detailed_res.full_title
     res.description = detailed_res.description
 
     return res
 
-def parse_result_list():
-
-    html = open(html_file, 'rb').read()
-
-    #html = get_recherche_result(2)
+def parse_html_search_result(html):
 
     soup = BeautifulSoup(html)
     annonce_list = soup.find_all('div', class_='bloc-vue-in')
 
-    print len(annonce_list)
-
-    annonce = annonce_list[0]
-
-    #print [annonce.prettify()]
-
-    title = ['titre', 'surface', 'prix', 'description']
-    table = []
-
-    #annonce_list = annonce_list[:2]
+    res = []    
 
     for annonce in annonce_list:
         res_an = parse_annonce_full(annonce)
 
-        #print res_an.full_title.encode('utf-8'), res_an.price.encode('utf-8')
-        #print res_an.description.encode('utf-8')
+        res.append(res_an)
 
-        titre_annonce = '<a href="%s">%s</a>'%(res_an.full_url, res_an.full_title)
+        #titre_annonce = '<a href="%s">%s</a>'%(res_an.full_url, res_an.full_title)
+        #line = [titre_annonce, res_an.surface, res_an.price, res_an.description]
 
-        line = [titre_annonce, res_an.surface, res_an.price, res_an.description]
-        table.append(line)
+    return res
 
-    #print [soup.prettify()]
-
-    html = make_html.make_table(title, table)
-    f = open('result.html', 'wb')
-    f.write(html.encode('utf-8'))
-    f.close()
-
-parse_result_list()
-
-#write_html_file()
-
-#parse_annonce_page()
-
-#print y
-
-#print u'\u2026'.decode('utf-8', 'replace')
+if __name__ == '__main__':
+    parse_result_list()
